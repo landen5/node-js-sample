637db8fcd25fdbaf841c406904d063922e02a0be
Landen Isacson <35053801+landen5@users.noreply.github.com>
2018-11-12 22:17:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('This is a web app hosted with Digital ocean. - Landen Isacson')
 })
 
 app.listen(app.get('port'), function() {
